580a26fd21e7a9a3a1b04e47a95088d3ccdbbbea
andoryu <andrewhowa@gmail.com>
2025-01-25 18:28:46 +1000

    libc printf tests

---
+++ 04.libc-printf/README.md
@@ -0,0 +1,1 @@
+Linkink assembly programs to libc - for printf, and perhaps other functions.
---
+++ 04.libc-printf/printer.asm
@@ -0,0 +1,28 @@
+section .data
+    message:    db "Hello, World", 0
+    fmt:        db "%s : %d. Pi = %4.3f", 10, 0
+    fnum:       dq 3.14159
+
+section .text
+    default rel
+    extern printf
+    global main
+
+main:
+    ; Create a stack-frame, re-aligning the stack to 16-byte alignment before calls
+    push rbp
+
+    lea	rdi, [fmt]
+    lea	rsi, [message]
+    mov rdx, 123
+    pxor xmm0, xmm0
+    movsd xmm0, qword [fnum]
+    mov	rax, 1          ; set number of float variables in vargs list.
+
+    ; Call printf
+    call printf wrt ..plt
+
+    pop	rbp		; Pop stack
+
+    mov	rax,0	; Exit code 0
+    ret			; Return
